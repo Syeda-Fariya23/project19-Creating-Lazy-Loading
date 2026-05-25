4052ea9c4556c829e277488f105a22781878bb8e
Syeda Fariya <syedafariya2003@gmail.com>
2026-05-25 16:06:42 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const progress = document.querySelector(".progress");
+const background = document.querySelector(".background");
+
+let load = 0;
+let int = setInterval(blurring, 30);
+function blurring() {
+    load++;
+    if (load > 99) {
+        clearInterval(int);
+    }
+    progress.innerHTML = `${load}%`
+    progress.style.opacity = scale(load, 0, 100, 1, 0);
+    background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+// const scale = (num, in_min, out_min, in_max, out_max) => {
+//     return ((num - in_min) * (out_max - out_min) )/ (in_max - in_min) + out_max;
+// };
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
